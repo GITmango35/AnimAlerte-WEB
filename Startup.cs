edd488d94b2bdb2e112b397b01d21f90ad555e3a
Nadia_F <naddou1@yahoo.com>
2022-03-14 13:13:10 -0400

    rajout Startup

--- Startup.cs
+++ Startup.cs
@@ -45,6 +45,10 @@ namespace AnimAlerte
             app.UseStaticFiles();
             app.UseRouting();
             app.UseSession();
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
             app.UseAuthorization();
             app.UseHttpsRedirection();
             app.UseEndpoints(endpoints =>
